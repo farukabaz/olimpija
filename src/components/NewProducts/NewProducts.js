bf1293e20d99e9eed28b32e05d3d037b6519b743
vsitarevic <vedad.sitarevic@gmail.com>
2021-02-17 15:44:45 +0100

    gray background new products

--- src/components/NewProducts/NewProducts.js
+++ src/components/NewProducts/NewProducts.js
@@ -4,26 +4,27 @@ import './NewProducts.css';
 const NewProducts = ({products}) => {
     return (
         <div className="products">
-            <div className="header-card-row">
-                <div className="bottom-right-header">
-                    NOVO U<br></br> PONUDI
+            <div className="products-all">
+                <div className="header-card-row">
+                    <div className="bottom-right-header">
+                        NOVO U<br></br> PONUDI
+                    </div>
                 </div>
+                {products.map((product) => 
+                    {
+                        return (
+                            <div className={'product-card ' + product.sex }>
+                                <img alt='parfem' className='product-img' src={product.parfemImg}/>
+                                <h2>{product.title}</h2>
+                                <h4>{product.oldPrice} KM</h4>
+                                <h3>{product.price} KM</h3>
+                            </div>
+                            )
+                        
+                    }
+                )}
+
             </div>
-            {products.map((product) => 
-                {
-                    return (
-                        <div className={'product-card ' + product.sex }>
-                            <img alt='parfem' className='product-img' src={product.parfemImg}/>
-                            <h2>{product.title}</h2>
-                            <h4>{product.oldPrice} KM</h4>
-                            <h3>{product.price} KM</h3>
-                        </div>
-                        )
-                    
-                }
-            )}
-
-
         </div>
     )
 }
